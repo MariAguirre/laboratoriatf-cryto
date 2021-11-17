dddf0bd5bd1f81c5af4333422b113595dd5c25d2
giovanna-crypto <giovannalopez826@gmail.com>
2021-11-16 14:16:21 -1100

    agregue maquetacion de page confirmación

---
+++ modules/confirmacion/BodyConfirmationBtn.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="
+  font monserrat
+  flex flex-col
+  justify-center
+  items-center
+  py-8 ">
+  <p class=" mt-8 py-8 px-32  items-center text-center"> Verificaremos tu operación. Puedes ver su estado en“Mis operaciones”</p>
+  <Button class="w-96 font-bold" text="IR A MIS OPERACIONES"></Button>
+  </div>
+</template>
+<script>
+import Button from "~/shared/ui/components/Button/Button.vue"
+
+export default {
+    components: {
+        Button,
+    },
+}
+</script>
+
+
+
